498e53ba27ea25fd992c302da0ad1333cf4dbb18
AlexGavrilova <sasha-17051994@yandex.ru>
2015-06-21 20:22:02 +0700

    Update 1_vijiner.py

--- 1_vijiner.py
+++ 1_vijiner.py
@@ -1,7 +1,7 @@
 import sys, os
 
 if len(sys.argv) != 5:
-    print "Incorrect arguments! Enter <input_file.txt> <output_file.txt> <1> (or <2>) <key>."
+    print "Invalid arguments! Enter <input_file.txt> <output_file.txt> <1> (or <2>) <key>."
     sys.exit(0)
 
 if not os.path.isfile(sys.argv[1]) :
