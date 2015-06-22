c7b0b74d2b091acdc97251c9088f5843acd4db2e
AlexGavrilova <sasha-17051994@yandex.ru>
2015-06-22 20:46:43 +0700

    Update 41_caesarGr.py

--- 41_caesarGr.py
+++ 41_caesarGr.py
@@ -1,23 +1,16 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import sys, os
 import Tkinter as tk
+import tkFileDialog 
 import tkMessageBox
 
 def caesar(InputFile,OutputFile, Key, EnDec):
-    if not os.path.isfile(InputFile):
-        tkMessageBox.showerror("Error!","File isn't found.")
-        sys.exit(0)
-
-    inputFile = open(InputFile,'r')
-    ch = inputFile.read(1)
+    ch = InputFile.read(1)
 
     outputFile = open(OutputFile, 'w')
 
     key = int(Key);
     if(key < 1 or key > 255):
-        tkMessageBox.showerror("Error!","Invalid key.")
+        tkMessageBox.showerror("Error!","Incorrect key.")
         sys.exit(0)
 
     while len(ch) != 0:
@@ -26,58 +19,53 @@ def caesar(InputFile,OutputFile, Key, EnDec):
         if (EnDec == '2'):
             brr = ((ord(ch) - key) + 256) % 256
         outputFile.write(chr(brr))
-        ch = inputFile.read(1)
+        ch = InputFile.read(1)
 
-    inputFile.close()
+    InputFile.close()
     outputFile.close()
     return True
 
 class SampleApp(tk.Tk):
     def __init__(self):
+        
         tk.Tk.__init__(self)
 	self.title('Caesar chiper')
-	self.geometry('300x200')
-	self.InFile = tk.Label(self, text = "Input file: ")
+        self.geometry('300x150')
 	self.OutFile = tk.Label(self, text = "Output file: ")
         self.Key = tk.Label(self, text = "Key(1,2,3...): ")
-        self.entryInFile = tk.Entry(self)
         self.entryOutFile = tk.Entry(self)
         self.entryKey = tk.Entry(self)
+        
         self.buttonEncrypt = tk.Button(self, text="Encrypt", command=self.on_button_encrypt)
         self.buttonDecrypt = tk.Button(self, text="Decrypt", command=self.on_button_decrypt)
-
-        self.InFile.place(x = 10, y = 10)
-        self.entryInFile.place(x = 100, y = 10)
-	self.OutFile.place(x = 10, y = 50)
-        self.entryOutFile.place(x = 100, y = 50)
-        self.Key.place(x = 10, y = 100)
-        self.entryKey.place(x = 100, y = 100)
-        self.buttonEncrypt.place(x = 20, y = 150)
-        self.buttonDecrypt.place(x = 120, y = 150)
+	
+	self.OutFile.place(x = 10, y = 10)
+	self.entryOutFile.place (x=100, y=10)
+        self.Key.place(x = 10, y = 50)
+        self.entryKey.place(x = 100, y = 50)
+        self.buttonEncrypt.place(x = 20, y = 100)
+        self.buttonDecrypt.place(x = 120, y = 100)
 
     def on_button_encrypt(self):
+        InputFile = tkFileDialog.askopenfile()
         Key = self.entryKey.get()
         if len(Key) == 0:
             sys.exit(0)
         OutputFile = self.entryOutFile.get()
 	if len(OutputFile) == 0:
-            sys.exit(0)
-        InputFile = self.entryInFile.get()
-	if len(InputFile) == 0:
             sys.exit(0)
         EnDec = '1'
         start = caesar(InputFile,OutputFile, Key, EnDec)
         tkMessageBox.showinfo("Success!","File is encrypted.")
 
     def on_button_decrypt(self):
+        InputFile = tkFileDialog.askopenfile()
         Key = self.entryKey.get()
         if len(Key) == 0:
             sys.exit(0)
         OutputFile = self.entryOutFile.get()
 	if len(OutputFile) == 0:
             sys.exit(0)
-        InputFile = self.entryInFile.get()
-	if len(InputFile) == 0:
             sys.exit(0)
         EnDec = '2'
         start = caesar(InputFile,OutputFile, Key, EnDec)
